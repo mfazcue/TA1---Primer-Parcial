1727eda885719b6696248f55cd0963086d21c7f6
mfazcue <mfazcue@gmail.com>
2021-05-31 20:14:52 -0300

    Hice cambios en guardar php

--- guardar.php
+++ guardar.php
@@ -1,11 +1,33 @@
 <?php
 
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$telefono = $_POST['telefono'];
-$edad = $_POST['edad'];
-$fechaDeNacimiento = $_POST['fechaNacimiento']; 
-$email = $_POST['email'];     
+    conectarBaseDatos();
+    guardarDatosEnBase($nombre, $apellido, $telefono, $edad, $fechaDeNacimiento, $email)
 
+    function conectarBaseDatos()  {
+		
+		$host = "localhost";
+		$usr = "mfazcue";
+		$pas = "1234";
+		$dbase = "parcial";
+		$db = new mysqli($host, $usr, $pas, $dbase);
+		 
+		if (mysqli_connect_error()) {
+			die('Error de conexión ('.mysqli_connect_errno().') '.mysqli_connect_error());
+		}
+
+		return $db;	
+
+    }
+
+    function guardarDatosEnBase ($nombre, $apellido, $telefono, $edad, $fechaDeNacimiento, $email) {
+
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $telefono = $_POST['telefono'];
+        $edad = $_POST['edad'];
+        $fechaDeNacimiento = $_POST['fechaNacimiento']; 
+        $email = $_POST['email'];     
+    
+    }
 
 ?>
