e373ba2a2c46c1c87fe8615ea572629a8a38410f
mfazcue <mfazcue@gmail.com>
2021-05-31 19:59:40 -0300

    Hice el archivo que toma los datos del formulario

---
+++ guardar.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$telefono = $_POST['telefono'];
+$edad = $_POST['edad'];
+$fechaDeNacimiento = $_POST['fechaNacimiento']; 
+$email = $_POST['email'];     
+
+
+?>
