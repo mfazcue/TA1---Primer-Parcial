03a44dbadcbe01b54478a942497f590ababdb5e3
mfazcue <mfazcue@gmail.com>
2021-06-01 22:37:09 -0300

    terminando validaciones php

--- validaciones.php
+++ validaciones.php
@@ -2,36 +2,36 @@
 
     function controlarDatos($nombre, $apellido, $telefono, $edad, $fechaDeNacimiento, $email) {
         if (!validarTextosVacios($nombre, $apellido, $telefono, $edad, $fechaDeNacimiento, $email)) {
-
+            return false;
         }
 
         if (!validarNumeros($edad)) {
-
+            return false;        
         }
 
         if (!nombreYapellidoDiferente($nombre, $apellido)) {
-                        
+            return false;                
         }        
 
         return true;
     }
 
-    function validarTextosVacios($datos){
-        if($datos['nombre'] === "" || $datos['apellido'] === "" || $datos['edad'] === "" || $datos['email'] === ""){
+    function validarTextosVacios($nombre, $apellido, $telefono, $edad, $fechaDeNacimiento, $email){
+        if($nombre === "" || $apellido === "" || $telefono === "" || $edad === "" || $fechaDeNacimiento === "" || $email === ""){
             return false;
         }
         return true;
     }
 
-    function validarNumeros($numero){
-        if(!is_numeric($numero)){
+    function validarNumeros($edad){
+        if(!is_numeric($edad)){
             return false;
         }
         return true;
     }
 
     function nombreYapellidoDiferente($nombre, $apellido){
-        if(!is_numeric($numero)){
+        if($nombre != $apellido){
             return false;
         }
         return true;
